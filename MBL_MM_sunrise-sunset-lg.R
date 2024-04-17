876524bf782c7fd0e38f5a0d28391ef99ffd6ccf
m-ball3 <mball3@uw.edu>
2024-04-17 16:54:50 -0700

    Updates sunrise-sunset.lg script to correctly access bioRad package

--- MBL_MM_sunrise-sunset-lg.R
+++ MBL_MM_sunrise-sunset-lg.R
@@ -1,3 +1,12 @@
 #Linear regression analysis (Dependent Var = #of interactions; Independent Var = time difference to sunrise/sunset)
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("rhdf5")
 install.packages("bioRad")
+
+library(bioRad)
+?sunrise
+
+?sunset
